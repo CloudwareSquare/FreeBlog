6645def43976946015fa65ca296ef5e48fd9690f
dacalin-mac <casale.candoit@gmail.com>
2017-12-05 20:07:18 +0100

    Rename BlogController

--- src/http/controllers/admin/PostsController.php
+++ src/http/controllers/admin/PostsController.php
@@ -7,7 +7,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
-class BlogController extends Controller
+class PostsController extends Controller
 {
     public function __construct()
     {
